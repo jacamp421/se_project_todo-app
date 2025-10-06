5faba5578cabd75803b99e3a144674bc7a042d0e
j <jacampoverde421@gmail.com>
2025-10-06 19:12:49 -0400

    Initial Commit

---
+++ compontents/FormValidator.js
@@ -0,0 +1,11 @@
+class FormValidator {
+    constructor(settings, formEl){
+    this._settings = settings;
+    this._formEl = formEl;
+    }
+    enableValidation(){
+
+    }
+}
+
+export default FormValidator;
